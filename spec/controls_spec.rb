7836ea796da1070c344311fb85a4f533f7dcbf89
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-04-02 18:28:41 +0300

    Added win? test

--- spec/controls_spec.rb
+++ spec/controls_spec.rb
@@ -1,6 +1,15 @@
-require_relative './../lib/board'
+require 'set'
+require_relative './../lib/controls'
 
 RSpec.describe Controls do
-  controls = Controls.new()
+  controls = Controls.new('player1', 'player2')
 
+  describe '#win?' do
+    it 'Positions are started at 1' do
+      expect(controls.win?([1, 2, 3])).to eql(true)
+      expect(controls.win?([1, 4, 2, 3])).to eql(true)
+      expect(controls.win?([2, 3])).to eql(false)
+      expect(controls.win?([2, 3, 7])).to eql(false)
+    end
+  end
 end
